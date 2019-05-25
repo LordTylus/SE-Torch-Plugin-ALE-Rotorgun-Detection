4e9707b795d996768375191dfae721636718db30
LordTylus <LordTylus@users.noreply.github.com>
2019-05-25 02:42:06 +0200

    Config and UI added

---
+++ RotorgunDetectorConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Torch;
+
+namespace ALE_Rotorgun_Detection {
+    public class RotorgunDetectorConfig : ViewModel {
+
+        private int _detachCooldown = 60 * 5; //5 Minutes
+        private int _loggingCooldown = 10; //10 Seconds
+        private int _minRotorGridCount = 4; //4 Rotors
+
+        public int DetachCooldown { get => _detachCooldown; set => SetValue(ref _detachCooldown, value); }
+
+        public int LoggingCooldown { get => _loggingCooldown; set => SetValue(ref _loggingCooldown, value); }
+
+        public int MinRotorGridCount { get => _minRotorGridCount; set => SetValue(ref _minRotorGridCount, value); }
+    }
+}
--- RotorgunDetectorPlugin.cs
+++ RotorgunDetectorPlugin.cs
@@ -2,16 +2,19 @@
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 using Torch;
 using Torch.API;
+using Torch.API.Plugins;
 using VRage.Scripting;
 
 namespace ALE_Rotorgun_Detection
 {
-    public class RotorgunDetectorPlugin : TorchPluginBase {
+    public class RotorgunDetectorPlugin : TorchPluginBase, IWpfPlugin {
 
         public static readonly Logger Log = LogManager.GetCurrentClassLogger();
         public static RotorgunDetectorPlugin Instance { get; private set; }
@@ -22,12 +25,39 @@ namespace ALE_Rotorgun_Detection
         public ConcurrentDictionary<long, CurrentCooldown> DetachCooldowns { get { return _detachCooldowns; } }
         public ConcurrentDictionary<long, CurrentCooldown> LoggingCooldowns { get { return _loggingCooldowns; } }
 
-        public long DetachCooldown { get { return 300 * 1000; } }
-        public long LoggingCooldown { get { return 10 * 1000; } }
-        public int MinRotorGridCount { get { return 4; } }
+        private Persistent<RotorgunDetectorConfig> _config;
+        public RotorgunDetectorConfig Config => _config?.Data;
+
+        public void Save() => _config.Save();
+
+        public long DetachCooldown { get { return Config.DetachCooldown * 1000; } }
+        public long LoggingCooldown { get { return Config.LoggingCooldown * 1000; } }
+        public int MinRotorGridCount { get { return Config.MinRotorGridCount; } }
+
+        private Control _control;
+        public UserControl GetControl() => _control ?? (_control = new Control(this));
 
         public override void Init(ITorchBase torch) {
+
             base.Init(torch);
+
+            var configFile = Path.Combine(StoragePath, "RotorgunDetector.cfg");
+
+            try {
+
+                _config = Persistent<RotorgunDetectorConfig>.Load(configFile);
+
+            } catch (Exception e) {
+                Log.Warn(e);
+            }
+
+            if (_config?.Data == null) {
+
+                Log.Info("Create Default Config, because none was found!");
+
+                _config = new Persistent<RotorgunDetectorConfig>(configFile, new RotorgunDetectorConfig());
+                _config.Save();
+            }
 
             var pgmr = new RotorgunDetectorManager(torch);
             torch.Managers.AddManager(pgmr);
